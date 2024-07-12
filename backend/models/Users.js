c010991c1f3692e6b2766d65c51b287cb8a33a4d
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-07-12 19:08:13 +0530

    f i saved recipes array

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -10,6 +10,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: "recipes" }],
 });
 
 export const UserModel = mongoose.model("user", UserSchema);
--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -1,13 +1,37 @@
 import express from "express";
-import mongoose from "mongoose";
 import { RecipeModel } from "../models/Recipes.js";
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
+    const response = await RecipeModel.find({});
+    res.json(response);
   } catch (error) {
     console.log(error);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const recipe = new RecipeModel(req.body);
+  try {
+    const response = await recipe.save();
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.put("/", async (req, res) => {
+  try {
+    const recipe = await RecipeModel.findById(req.body.recipeId);
+    const user = await UserModel.findById(req.body.userId);
+    user.savedRecipes.push(recipe);
+    await user.save();
+    res.json({ savedRecipes: user.savedRecipes });
+  } catch (error) {
+    res.json(error);
   }
 });
 
